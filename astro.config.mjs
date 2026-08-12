5b46d951d0dadc2834da42a782f534e24c95c47c
pakelcomedy <releaseonegit@gmail.com>
2026-08-12 20:45:05 +0700

    Initial commit: TIKET SURGA™

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://ONEdart.github.io/TiketSurga',
+  base: '/TiketSurga',
+  server: {
+    port: 3000,
+  },
+});
